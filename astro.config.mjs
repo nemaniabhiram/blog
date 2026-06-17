a6fc87ccc61d34e9398a5fba07b7d5f49ef8bfd6
nemaniabhiram <66569953+nemaniabhiram@users.noreply.github.com>
2026-06-18 00:04:37 +0530

    feat: new color scheme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      themes: {
-        light: 'one-light',
-        dark: 'one-dark-pro',
-      },
+      theme: 'github-dark',
     },
   },
 });
